f2b77e67e595218a920bc526f3d96657eb6bb79b
bsh-ko <ssuh3872@gmail.com>
2024-09-09 14:07:58 +0900

    3항연산자 연습

--- 02-javascript-basic/02-4-control-flow/test3-tenary/main.js
+++ 02-javascript-basic/02-4-control-flow/test3-tenary/main.js
@@ -3,4 +3,14 @@
 let data = 8
 let result; //undefined
 
+if( data % 4 == 0 ) {
+    result = '4의 배수입니다'
+} else {
+    result = '4의 배수가 아닙니다'
+}
+console.log(result)
 
+//위 코드를 3항연산자로
+let result2 = (data % 4 == 0 ) ? "4의 배수입니다." : "4의 배수가 아닙니다."
+
+//참 거짓으로 나뉠때는 삼항연산자로 줄여서 쓰는 것이 편함 
