2d148b8e2c9654ad3e250164d78e7c4ee3dc215e
bsh-ko <ssuh3872@gmail.com>
2024-09-09 13:15:01 +0900

    javascript control flow - if

---
+++ 02-javascript-basic/02-4-control-flow/quiz/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let age = prompt('나이를 입력해주세요.')
+
+if( age == null ) {
+    console.log('입력을 취소하였습니다.')
+} else if (isNaN(age) == true) {
+    console.log('숫자가 아닌 문자를 입력하셨습니다.')
+} else if ( age < 0 || age >= 100 ) {
+    console.log('1이상, 100이하의 숫자를 입력해야합니다.')
+} else if (age < 10) {
+    console.log('어린이군요')
+} else if (age >= 10 && age < 20) {
+    console.log('청소년이군요')
+} else if (age >= 20) {
+    console.log('어른이군요')
+} 
