b3e6dd46d12813947c4c5e8bc7cb4c0bfb9d6cf2
bsh-ko <ssuh3872@gmail.com>
2024-09-13 11:50:45 +0900

    배열 공부

---
+++ 03-implicit-object/03-1-javascript/test1-array-basic/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// [] 기법으로 배열선언
+let product1 = ['book1', 'book2']
+let product2 = []
+
+//instanceof : 연산자 -> 결과값은 true/false
+//왼쪽 객체가 오른 쪽 타입인가? 라는 뜻
+console.log(product1 instanceof Array) //true
+console.log(product2 instanceof Array) //true
+
+//원래는 new Array()로 배열 선언
+let product3 = new Array('book3', 'book4')
+let product4 = new Array() //빈 배열 객체
+
+
+
+
+
