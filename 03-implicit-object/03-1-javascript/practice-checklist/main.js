39c2dee9cc3bfade477ffd4798f2d427023046e4
bsh-ko <ssuh3872@gmail.com>
2024-09-16 22:17:07 +0900

    feat. 추가 버튼

--- 03-implicit-object/03-1-javascript/practice-checklist/main.js
+++ 03-implicit-object/03-1-javascript/practice-checklist/main.js
@@ -1,1 +1,21 @@
 "use strict"
+
+let todoArray = []
+
+let inputNode = document.getElementById('todoList')
+let resultNode = document.getElementById('todoList_result')
+
+// todoList를 출력해주는 함수
+function printResult(array) {
+    let result = ''
+    array.forEach((value, index) => {
+        result += `<lable>${value}</lable>`
+    })
+    resultNode.innerHTML = result
+}
+
+function addList() {
+    let todo = inputNode.value
+    todoArray.push(todo)
+    printResult(todoArray)
+}
